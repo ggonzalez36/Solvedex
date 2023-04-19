759e6b1d5c194d17960808d060668e2a2ba2470a
gustavo-gonzalez-adl <106705816+gustavo-gonzalez-adl@users.noreply.github.com>
2023-04-19 13:37:34 -0500

    Adding create post component

--- front-angular/src/app/components/add-post/add-post.component.ts
+++ front-angular/src/app/components/add-post/add-post.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Posts } from 'src/app/models/posts.model';
+import { PostsService } from 'src/app/service/posts.service';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
   styleUrls: ['./add-post.component.css']
 })
-export class AddPostComponent {
+export class AddPostComponent implements OnInit {
+
+  post: Posts = {
+    title: '',
+    body: '',
+   
+  };
+  submitted = false;
+
+  constructor(private postsService: PostsService) { }
+
+  ngOnInit(): void {
+  }
+
+  savePost(): void {
+    const data = {
+      title: this.post.title,
+      description: this.post.body
+    };
+
+  
+    this.postsService.create(data)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.submitted = true;
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  newTutorial(): void {
+    this.submitted = false;
+    this.post = {
+      title: '',
+      body: '',
+    };
+  }  
 
 }
--- front-angular/src/app/service/posts.service.ts
+++ front-angular/src/app/service/posts.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Posts } from '../models/posts.model';
 
+const baseUrl = 'http://localhost:8080/api/v1/post';
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get(id: any): Observable<Posts> {
+    return this.http.get<Posts>(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(`${baseUrl}/create`, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/update`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+
 }
